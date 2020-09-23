bde13c6266fe77897180fdf95b257f7f187e9860
DELL <DELL@DESKTOP-GN0RMSF>
2020-09-23 15:21:09 +0530

    BookStore updated with mockito

--- https/github.com/akshitha917/BootcampProject-Book-Store-.git/OnlineBookStore/src/test/java/com/capg/demo/OnlineBookStoreApplicationTests.java
+++ https/github.com/akshitha917/BootcampProject-Book-Store-.git/OnlineBookStore/src/test/java/com/capg/demo/OnlineBookStoreApplicationTests.java
@@ -1,52 +1,36 @@
 package com.capg.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import com.cg.bookstore.OnlineBookStoreApplication;
 import com.cg.bookstore.entity.Category;
 import com.cg.bookstore.repository.CategoryRepository;
 import com.cg.bookstore.service.CategoryService;
 
-@SpringBootTest(classes=OnlineBookStoreApplication.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = OnlineBookStoreApplication.class)
 @TestInstance(Lifecycle.PER_CLASS)
-class OnlineBookStoreApplicationTests {
+public class OnlineBookStoreApplicationTests {
 	
-    @Autowired
-	static CategoryService service;	
-    @Autowired
-	CategoryRepository repo;
-	
-	@Autowired
-	static Category details;
-	
-	@BeforeEach
-	public void init() {
-		service = new CategoryService();
-		details = new Category(1,"Comic");
-
-	}
-
-	@Test
-	public void testAddAccount() {
-		Category detailsTest = repo.save(details);
-		assertEquals(detailsTest.getCategoryName(), details.getCategoryName());
-	}
+	@InjectMocks
+	CategoryService service;	
+    @Mock
+    CategoryRepository repo;
 	
 	@Test
-	public void testEditAccount() {
-		Category detailsTest = repo.save(details);
-		assertEquals(detailsTest.getCategoryName(),detailsTest.getCategoryName());
+	public void testCreateCategory() {
+		Category details=new Category();
+		details.setCategoryName("comic");
+		Mockito.when(repo.save(details)).thenReturn(details);
+		assertEquals(details, service.createCategory(details));
 	}
-	
-	@Test
-	public void testList() {
-		assertEquals(true, true);
-	}
-
 
 }
